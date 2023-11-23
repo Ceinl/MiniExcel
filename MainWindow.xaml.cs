2393b701b63220a8ec5ee2fe4ead8e5490312fdc
Dima Slyva <slyva.dima@gmail.com>
2023-11-23 15:54:48 +0100

    -Rows and columne dynamic add

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,7 +24,46 @@ namespace MiniExcel
         {
             InitializeComponent();
         }
+        int TextRow = 0;
+        private void AddColumne(object sender, RoutedEventArgs e)
+        {            
+                TextBox newTextBox = new TextBox
+                {
+                    Width = 30,
+                    Height = 30,
 
-       
+                };
+
+        
+            ColumnDefinition newColumn = new ColumnDefinition();
+            ItemContainer.ColumnDefinitions.Add(newColumn);
+
+            Grid.SetColumn(newTextBox, ItemContainer.ColumnDefinitions.Count - 1);
+
+            ItemContainer.Children.Add(newTextBox);
+        }
+
+        private void btAddRow_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox newTextBox = new TextBox
+            {
+                Width = 30,
+                Height = 30,
+
+            };
+
+           
+            
+
+            RowDefinition newRowDefinition = new RowDefinition();
+            ItemContainer.RowDefinitions.Add(newRowDefinition);
+
+            Grid.SetRow(newTextBox, ItemContainer.RowDefinitions.Count - 1);
+
+            ItemContainer.Children.Add(newTextBox);
+        }
     }
-}
+    }
+
+
+
